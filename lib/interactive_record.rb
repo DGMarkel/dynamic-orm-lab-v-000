e3e828461be7e20d44ec3ae866e15608eb0a77f1
David Markel <davidgmarkel@gmail.com>
2018-05-01 16:59:47 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,11 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'pry'
+
 class InteractiveRecord
 
   def self.table_name
-    table_name = self.downcase.pluralized
-    binding.pry
+    self.to_s.downcase.pluralize
   end
 
 end
