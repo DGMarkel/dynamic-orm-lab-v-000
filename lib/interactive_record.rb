0d7e6f6a4398ad0c2086fa5d09140581761967fc
David Markel <davidgmarkel@gmail.com>
2018-05-01 17:50:35 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,6 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
     DB[:conn].execute(sql, attribute.keys.first.to_s, attribute.values.first)
   end
